18680faf64cd1150a0e407e1c48e48d697ac1c07
Quentin Torroba <torroba.q@gmail.com>
2023-08-25 22:01:56 +0200

    Fix: styling

--- setup.py
+++ setup.py
@@ -1,16 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pymd',
-    version='0.1',
+    name="pymd",
+    version="0.1",
     packages=find_packages(),
-    install_requires=[
-        'matplotlib'
-    ],
-    entry_points={
-        'console_scripts': [
-            'pymd=pymd.__main__:main'
-        ]
-    }
+    install_requires=["matplotlib"],
+    entry_points={"console_scripts": ["pymd=pymd.__main__:main"]},
 )
-
